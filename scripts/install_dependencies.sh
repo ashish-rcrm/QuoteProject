1746aa3f4a03489d54e6074e9590b6be505d59dc
ashish-rcrm <ashishbhatia@recruitcrm.io>
2022-04-11 17:40:07 +0530

    made changes to the scripts

---
+++ scripts/install_dependencies.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+yum install -y httpd
+# this installs apache web server
+# we do not need to write sudo here as we have written runas : root 
---
+++ scripts/validate_service.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# this is a health check that things are working properly
+curl -v --slient localhost:80 2>&1 
