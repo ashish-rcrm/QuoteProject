db4f85e2df9f6e00194f4e0804c9ab3d0fa260d4
ashish-rcrm <102274953+ashish-rcrm@users.noreply.github.com>
2022-04-17 11:57:59 +0530

    Update after_install.sh

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,5 +1,5 @@
 #/!bin/bash
-curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
-sudo yum install nodejs
-npm i @vue/cli-service
-cd /var/www/html
+# curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
+# sudo yum install nodejs
+# npm i @vue/cli-service
+# cd /var/www/html
