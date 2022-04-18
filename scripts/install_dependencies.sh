eae8d740aaa29611e9afbee95f2fb34d12e788f1
ashish-rcrm <102274953+ashish-rcrm@users.noreply.github.com>
2022-04-18 12:49:33 +0530

    Update install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -5,6 +5,9 @@ wget https://aws-codedeploy-us-east-1.s3.us-east-1.amazonaws.com/latest/install
 chmod +x ./install
 sudo ./install auto 
 # sudo service codedeploy-agent status
+sudo yum install -y gcc-c++ make 
+curl -sL https://rpm.nodesource.com/setup_14.x | sudo -E bash - 
+sudo yum install -y nodejs
 sudo yum install httpd -y
 # this installs apache web server
 # we do not need to write sudo here as we have written runas : root 
