b0fe59b93eeb51060df52798f1fc3760f969b6fe
ashish-rcrm <102274953+ashish-rcrm@users.noreply.github.com>
2022-04-18 12:37:37 +0530

    Update install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+sudo yum update
+sudo yum install -y ruby wget
+wget https://aws-codedeploy-us-east-1.s3.us-east-1.amazonaws.com/latest/install 
+chmod +x ./install
+sudo ./install auto 
+# sudo service codedeploy-agent status
 sudo yum install httpd -y
 # this installs apache web server
 # we do not need to write sudo here as we have written runas : root 
